1728dcbb8a7f13fdd36f7914d879eff5edae61ff
Jérôme Tea <jerometea@gmail.com>
2021-09-06 13:13:36 +0200

    .

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database : 'mongodb+srv://user:user@cluster0.lkmxe.mongodb.net/test?retryWrites=true&w=majority'
+    database : 'mongodb://user:user@cluster0.lkmxe.mongodb.net/test?retryWrites=true&w=majority'
     
 }
